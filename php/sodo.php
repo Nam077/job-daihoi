c647402f13579e5679edaf799de5a0d0a7f9dd2c
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2022-05-30 11:07:27 +0700

    Update loi di

--- php/sodo.php
+++ php/sodo.php
@@ -8,9 +8,21 @@
         }
     }
     $index = 0;
+    echo "<tr>";
+    echo "<td colspan='".($n + 1)."' class='stage'>Sân khấu </td>";
     for ($i = 0; $i < $m; $i++) {
         echo "<tr>";
-        for ($j = 1; $j <= $n; $j++) {
+        for ($j = 1; $j <= $n / 2; $j++) {
+            $seat = chr($i + 65)."".$j;
+            if ($seat == $_GET["seat"])
+                echo "<td id='".$seat."' class='seat'>".$seat."</td>";
+            else
+                echo "<td id='".$seat."' class='normal'>".$seat."</td>";
+        }
+        if ($i == 0)
+            echo "<td rowspan = '18'> Lối </td>";
+
+        for ($n / 2 + 1; $j <= $n; $j++) {
             $seat = chr($i + 65)."".$j;
             if ($seat == $_GET["seat"])
                 echo "<td id='".$seat."' class='seat'>".$seat."</td>";
